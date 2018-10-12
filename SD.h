1d9ef11c43e908a866ccb14ed08d82f1c3c162d5
Brett Heinkel <bheinks@gmail.com>
2018-10-12 02:54:29 -0500

    Implement read_block

--- SD.h
+++ SD.h
@@ -4,8 +4,9 @@
 #include "main.h"
 
 #define ILLEGAL_COMMAND 0xE0
-#define COMM_ERROR 0x83
 #define SD_INIT_ERROR 0xE1
+#define COMM_ERROR 0xE3
+#define DATA_ERROR 0xE4
 
 // SD card commands 
 #define CMD0 0
@@ -19,5 +20,6 @@
 uint8_t send_command(uint8_t command, uint32_t argument);
 uint8_t receive_response(uint8_t num_bytes, uint8_t *byte_array);
 uint8_t SD_card_init(void);
+uint8_t read_block(uint16_t num_bytes, uint8_t * byte_array);
 
 #endif
