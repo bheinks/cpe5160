e67e7431f83af628b7e1b3a010d16033d5f02463
asarinn <mproemsey@gmail.com>
2018-11-11 16:57:18 -0600

    read functions

--- FAT.c
+++ FAT.c
@@ -12,14 +12,44 @@ sbit yellow = P2^5;
 sbit red = P2^4;
 
 uint8_t read8 (uint16_t offset, uint8_t * array_name){
-    
+   uint8_t idata ret;
+
+   ret = 0;
+   offset&=0x1FF; 
+ 
+   ret=*(array_name + offset);
+   return ret;
 }
 
 uint16_t read16 (uint16_t offset, uint8_t * array_name){
-    
+   uint16_t idata ret;
+   uint8_t idata index, temp;
+
+   offset&=0x1FF;  
+   ret=0;
+
+
+	for (index = 0; index < 3; index++){
+		temp = *(array_name  +  offset  +  (2-index));
+		ret = ret << 8;
+		ret = ret | temp;
+	}
+
+   return ret;
 }
 
 uint32_t read32 (uint16_t offset, uint8_t * array_name){
-    
+   uint32_t idata ret;
+   uint8_t idata temp, index;
+
+   ret=0;
+   offset&=0x1FF;  
+
+   for(index=0;index<4;index++){   
+       temp=*(array_name + offset + (3-index));
+       ret=ret<<8;
+       ret|=temp;
+   }
+   return ret;
 }
 
